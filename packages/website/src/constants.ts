9b617064310b8d415d2be9e4b159b79b20b8e614
cem <cem@github>
2026-03-29 13:06:53 +0300

    use github release

--- packages/website/src/constants.ts
+++ packages/website/src/constants.ts
@@ -1,8 +1,7 @@
 // Demo build (auto-init with demo LLM, for quick testing)
-export const CDN_DEMO_URL = 'https://cemdrk.github.io/page-agent/page-agent.demo.js'
+export const CDN_DEMO_URL = 'https://github.com/cemdrk/page-agent/releases/download/demo-2/page-agent.demo.js'
 
 
 // Demo LLM for website testing (homepage quick trial uses flash)
 export const DEMO_MODEL = 'qwen3.5-flash'
 export const DEMO_BASE_URL = 'https://page-ag-testing-ohftxirgbn.cn-shanghai.fcapp.run'
-// export const DEMO_API_KEY = ''
